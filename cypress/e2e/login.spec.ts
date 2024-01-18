50a538b2dca1bc2fc0090ce11ac8876025e3f849
DESKTOP-K2GASL6\Angel <carmenFGDev@gmail.com>
2024-01-18 22:38:46 +0100

    Testing login

--- cypress/e2e/login.spec.ts
+++ cypress/e2e/login.spec.ts
@@ -17,25 +17,44 @@ describe('Login specs', () => {
 
     // Act
     cy.visit('/');
-    cy.findAllByTestId('password').click();
+    cy.get('input[name="password"]').click();
 
     // Assert
-    cy.findAllByTestId('password').should('have.focus');
+    cy.get('input[name="password"]').should('have.focus');
   });
    it('should show an alert with a message when type invalid credentials', () => {
     // Arrange
     const user = 'admin';
     const password = '1234';
 
-     // Act
+    // Act
     cy.visit('/');
     cy.get('input[name="user"]').as('userInput');
     cy.get('@userInput').type(user);
     cy.get('input[name="password"]').as('passwordInput');
     cy.get('@passwordInput').type(password);
+    cy.findByRole('button', { name: 'Login' }).click();
 
     // Assert
     cy.get('@userInput').should('have.value', user);
     cy.get('@passwordInput').should('have.value', password);
+    cy.findByText('Usuario y/o password no válidos').should('be.visible');
+  });
+
+   it('should navigate to hotels url when type valid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = 'test';
+
+    // Act
+    cy.visit('/');
+    cy.get('input[name="user"]').as('userInput');
+    cy.get('@userInput').type(user);
+    cy.get('input[name="password"]').as('passwordInput');
+    cy.get('@passwordInput').type(password);
+    cy.findByRole('button', { name: 'Login' }).click();
+
+    // Assert
+    cy.url().should('equal', 'http://localhost:8080/#/submodule-list');
   });
 });
